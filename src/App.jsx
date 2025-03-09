b2ab9143f17e5fc76ceec038d39561a4a80d4d28
Shoity <lucasshoity@gmail.com>
2025-03-09 20:38:42 -0300

    Feat: Create default theme and initialize of  component Header.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { ThemeProvider } from "styled-components"
+import { Header } from "./Components/Header"
+import { defaultTheme } from "./Styles/Themes/default"
+import { GlobalStyle } from "./Styles/global"
+
 function App() {
   
-  return <h1>Hello</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+      <Header/>
+
+    </ThemeProvider>
+  )
 }
 
 export default App
---
+++ src/Components/Header/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const HeaderContent = styled.header`
+    width: 100%;
+    max-width: 1120px;
+    background: ${(props) => props.theme['base-profile']};
+`
